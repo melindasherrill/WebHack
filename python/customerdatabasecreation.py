efe828874e673118796cbddedb1e93aebd0f83f4
melinda <melindasherrill@socal.rr.com>
2018-05-02 00:26:30 -0700

    flake8 fixes

--- python/customerdatabasecreation.py
+++ python/customerdatabasecreation.py
@@ -4,23 +4,31 @@
 import sqlite3
 
 # Connect to database and point to it
-conn = sqlite3.connect("../dbs/cust.sqlite");
+conn = sqlite3.connect("../dbs/cust.sqlite")
 c = conn.cursor()
+
 
 # Create Database Table
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS customers(customerid REAL, fname TEXT, lname TEXT, company TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS customers'
+              + '(customerid REAL, fname TEXT, lname TEXT, company TEXT)')
 
 
 # Create Entries (noticed where the names are from?)
 def data_entry():
     c.execute("DELETE FROM customers")
-    c.execute("INSERT INTO customers VALUES(1, 'Rachel', 'Green', 'Ralph Lauren')")
-    c.execute("INSERT INTO customers VALUES(1982735, 'Michael', 'Fahy', 'Chapman')")
-    c.execute("INSERT INTO customers VALUES(2, 'Monica', 'Geller', 'Chez Geller')")
-    c.execute("INSERT INTO customers VALUES(3, 'Ross', 'Geller', 'Professor')")
-    c.execute("INSERT INTO customers VALUES(4, 'Chandler', 'Bing', 'Data Analyist')")
-    c.execute("INSERT INTO customers VALUES(5, 'Phoebe', 'Buffay', 'Psychic')")
+    c.execute("INSERT INTO customers VALUES(1, 'Rachel', "
+              + " 'Green', 'Ralph Lauren')")
+    c.execute("INSERT INTO customers VALUES(1982735, "
+              + "'Michael', 'Fahy', 'Chapman')")
+    c.execute("INSERT INTO customers VALUES(2, "
+              + "'Monica', 'Geller', 'Chez Geller')")
+    c.execute("INSERT INTO customers VALUES(3, 'Ross', "
+              + " 'Geller', 'Professor')")
+    c.execute("INSERT INTO customers VALUES(4, 'Chandler', "
+              + " 'Bing', 'Data Analyist')")
+    c.execute("INSERT INTO customers VALUES(5, 'Phoebe', "
+              + " 'Buffay', 'Psychic')")
 
     # Commit entries and close database connection
     conn.commit()
--- python/dbusercreation.py
+++ python/dbusercreation.py
@@ -3,23 +3,27 @@
 import sqlite3
 
 # Make Connection to database (creates the file if not already exists)
-conn = sqlite3.connect("users.sqlite");
+conn = sqlite3.connect("users.sqlite")
 c = conn.cursor()
+
 
 # Creates a users table
 def create_table():
-	c.execute('CREATE TABLE IF NOT EXISTS users(id REAL, username TEXT, password TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS users(id REAL,'
+              + ' username TEXT, password TEXT)')
+
 
 # Adds user entries to the users table for the users database
 def data_entry():
-	c.execute("DELETE FROM users")
-	c.execute("INSERT INTO users VALUES(1, 'admin', 'youwillneverguessthispassword')")
-	c.execute("INSERT INTO users VALUES(1, 'lindy', 'passw0rd')")
+    c.execute("DELETE FROM users")
+    c.execute("INSERT INTO users VALUES(1, 'admin'," +
+              " 'youwillneverguessthispassword')")
+    c.execute("INSERT INTO users VALUES(2, 'lindy', 'passw0rd')")
 
-	# Commit entries and close connections
-	conn.commit()
-	c.close()
-	conn.close()
+    # Commit entries and close connections
+    conn.commit()
+    c.close()
+    conn.close()
 
 # Run the functions
 create_table()
